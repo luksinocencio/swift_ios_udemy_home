17e70118dcb4290dbfd4fc1ccce50b95c3e47894
luksinocencio <luksinocencio@gmail.com>
2024-06-07 00:56:57 -0300

    add MainBannerView

--- ios-udemy-home/SwiftUIView/MainBannerView.swift
+++ ios-udemy-home/SwiftUIView/MainBannerView.swift
@@ -1,18 +1,40 @@
-//
-//  MainBannerView.swift
-//  ios-udemy-home
-//
-//  Created by Lucas Inocencio on 06/06/24.
-//
-
 import SwiftUI
 
 struct MainBannerView: View {
+    let imageLink: String
+    let title: String
+    let caption: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            AsyncImage(url: URL(string: imageLink)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 160)
+                    .clipped()
+            } placeholder: {
+                PlaceholderImageView()
+                    .frame(height: 160)
+            }
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.system(size: 24, weight: .bold, design: .serif))
+                Text(caption)
+                    .font(.system(size: 12))
+                    .fixedSize(horizontal: false, vertical: true)
+                    .lineLimit(2)
+            }
+            .padding(.leading, 20)
+        }
     }
 }
 
 #Preview {
-    MainBannerView()
+    MainBannerView(
+        imageLink: "https://plus.unsplash.com/premium_photo-1661373704604-7c4d230c8928?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Learning that fits",
+        caption: "Skills for your present and future"
+    )
 }
--- ios-udemy-home/SwiftUIView/PlaceholderImageView.swift
+++ ios-udemy-home/SwiftUIView/PlaceholderImageView.swift
@@ -1,15 +1,9 @@
-//
-//  PlaceholderImageView.swift
-//  ios-udemy-home
-//
-//  Created by Lucas Inocencio on 06/06/24.
-//
-
 import SwiftUI
 
 struct PlaceholderImageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Rectangle()
+            .foregroundColor(Color.gray).opacity(0.3)
     }
 }
 
