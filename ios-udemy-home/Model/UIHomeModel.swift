dfece87d104a7b3f6d2d48c79c183d4b4af83049
luksinocencio <luksinocencio@gmail.com>
2024-06-07 00:39:50 -0300

    add ui model

--- ios-udemy-home/Model/UIHomeModel.swift
+++ ios-udemy-home/Model/UIHomeModel.swift
@@ -1,8 +1,59 @@
-//
-//  UIHomeModel.swift
-//  ios-udemy-home
-//
-//  Created by Lucas Inocencio on 06/06/24.
-//
-
 import Foundation
+
+struct HomeUIModel: Hashable {
+    struct SectionModel: Hashable {
+        let section: Section
+        let body: [Item]
+    }
+    
+    enum Section: Hashable {
+        case mainBanner(id: String)
+        case textHeader(id: String)
+        case courseSwimlane(id: String)
+        case udemyBusinessBanner(id: String)
+        case categories(id: String)
+        case featuredCourse(id: String)
+    }
+    
+    enum Item: Hashable {
+        case mainBanner(
+            id: String,
+            imageLink:
+                String,
+            title: String,
+            caption: String
+        )
+        case course(
+            id: String,
+            imageLink: String,
+            title: String,
+            author: String,
+            rating: Double,
+            reviewCount: Int,
+            price: Decimal,
+            tag: String
+        )
+        case textHeader(
+            id: String,
+            text: String,
+            highlightedText: String?
+        )
+        case udemyBusinessBanner(
+            id: String,
+            link: String
+        )
+        case categoryScroller(
+            id: String,
+            titles: [String]
+        )
+        case featuredCourse(
+            id: String,
+            imageLink: String,
+            title: String,
+            author: String,
+            rating: Double,
+            reviewCount: Int,
+            price: Decimal
+        )
+    }
+}
