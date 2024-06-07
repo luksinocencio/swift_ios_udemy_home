3b684c9dcc22b8a0422071f6c8410e864fb4de4f
luksinocencio <luksinocencio@gmail.com>
2024-06-07 08:55:10 -0300

    add MainBannerCollectionViewCell

--- ios-udemy-home/View/MainBannerCollectionViewCell.swift
+++ ios-udemy-home/View/MainBannerCollectionViewCell.swift
@@ -1,1 +1,26 @@
-import Foundation
+import SwiftUI
+import SnapKit
+
+final class MainBannerCollectionViewCell: UICollectionViewCell {
+    private var hostingController: UIHostingController<MainBannerView>!
+    
+    func configure(
+        imageLink: String,
+        title: String,
+        caption: String
+    ) {
+        guard hostingController == nil else { return }
+        let mainBannerView = MainBannerView(
+            imageLink: imageLink,
+            title: title,
+            caption: caption
+        )
+        hostingController = UIHostingController(rootView: mainBannerView)
+        guard let hostingController = hostingController else { return }
+        addSubview(hostingController.view)
+        hostingController.view.clipsToBounds = true
+        hostingController.view.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
