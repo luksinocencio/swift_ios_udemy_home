41c011c4fec9cbb624d22a3c807e2dcaa446c53b
luksinocencio <luksinocencio@gmail.com>
2024-07-10 16:44:16 -0300

    feat: add CourseCollectionViewCell

--- ios-udemy-home/View/CourseCollectionViewCell.swift
+++ ios-udemy-home/View/CourseCollectionViewCell.swift
@@ -1,8 +1,44 @@
-//
-//  CourseCollectionViewCell.swift
-//  ios-udemy-home
-//
-//  Created by Lucas Inocencio on 06/06/24.
-//
+import SwiftUI
+import SnapKit
 
-import Foundation
+final class CourseCollectionViewCell: UICollectionViewCell {
+    private var hostingController: UIHostingController<CourseView>!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .gray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(
+        imageLink: String,
+        title: String,
+        author: String,
+        rating: Double,
+        reviewCount: Int,
+        price: Decimal,
+        tag: String
+    ) {
+        guard hostingController == nil else { return }
+        
+        let courseView = CourseView(
+            imageLink: imageLink,
+            title: title,
+            author: author,
+            rating: rating,
+            reviewCount: reviewCount,
+            price: price,
+            tag: tag
+        )
+        
+        hostingController = UIHostingController(rootView: courseView)
+        addSubview(hostingController.view)
+        hostingController.view.clipsToBounds = true
+        hostingController.view.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
